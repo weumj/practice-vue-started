ece75ad4a5c96a70efddb25978a35043bc5ed4df
Myungjin Kim <weumj1@gmail.com>
2020-04-23 16:52:59 +0900

    4 - list & conditional

--- src/components/heros.vue
+++ src/components/heros.vue
@@ -4,149 +4,111 @@
       <h2 class="title">Heroes</h2>
     </div>
     <div class="columns">
-      <div class="column is-8">
-        <div class="card edit-detail">
-          <header class="card-header">
-            <p class="card-header-title">{{ hero.firstName }}</p>
-          </header>
-          <div class="card-content">
-            <div class="content">
-              <div class="field">
-                <label class="label" for="id">id</label>
-                <label class="input" id="id" readonly> {{ hero.id }}</label>
-              </div>
-              <div class="field">
-                <label class="label" for="firstName">first name</label>
-                <input class="input" id="firstName" v-model="hero.firstName" />
-              </div>
-              <div class="field">
-                <label class="label" for="lastName">last name</label>
-                <input class="input" id="lastName" v-model="hero.lastName" />
-              </div>
-              <div class="field">
-                <label class="label" for="description">description</label>
-                <textarea
-                  class="input"
-                  id="description"
-                  type="text"
-                  v-model="hero.description"
+      <div class="column is-3">
+        <header class="card-header">
+          <p class="card-header-title">heroes list</p>
+        </header>
+        <ul class="list is-hoverable">
+          <li v-for="hero in heroes" v-bind:key="hero.id">
+            <a
+              class="list-item"
+              :class="{ 'is-active': selectedHero === hero }"
+              @click="selectedHero = hero"
+              ><span>{{ hero.firstName }}</span></a
+            >
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="columns" v-if="selectedHero">
+      <div class="column is-3">
+        <header class="card-header">
+          <p class="card-header-title">{{ selectedHero.firstName }}</p>
+        </header>
+        <div class="card-content">
+          <div class="content">
+            <div class="field">
+              <label class="label" for="id">id</label>
+              <label class="input" id="id" readonly>
+                {{ selectedHero.id }}
+              </label>
+            </div>
+            <div class="field">
+              <label class="label" for="firstName">first name</label>
+              <input
+                class="input"
+                id="firstName"
+                v-model="selectedHero.firstName"
+              />
+            </div>
+            <div class="field">
+              <label class="checkbox" for="show"
+                >show more
+                <input
+                  class="is-primary"
+                  id="show"
+                  type="checkbox"
+                  v-model="showMore"
                 />
-              </div>
-              <div class="field">
-                <label class="label">cape color</label>
-                <label class="radio" for="color-red">
-                  <input
-                    type="radio"
-                    id="color-red"
-                    value="red"
-                    v-model="hero.capeColor"
-                  />
-                  red
-                </label>
-                <label class="radio" for="color-blue">
-                  <input
-                    type="radio"
-                    id="color-blue"
-                    value="blue"
-                    v-model="hero.capeColor"
-                  />
-                  blue
-                </label>
-                <label class="radio" for="color-green">
-                  <input
-                    type="radio"
-                    id="color-green"
-                    value="green"
-                    v-model="hero.capeColor"
-                  />
-                  green
-                </label>
-                <div
-                  class="color-line"
-                  :style="{ 'background-color': hero.capeColor }"
-                ></div>
-              </div>
-              <div class="field">
-                <label for="power">
-                  super power
-                  <div class="select is-primary">
-                    <select
-                      id="power"
-                      v-model="hero.power"
-                      @keyup.esc="clearPower"
-                      :class="{ invalid: !hero.power }"
-                    >
-                      <option disabled value>Please select one</option>
-                      <option>Speed</option>
-                      <option>Flight</option>
-                      <option>Strength</option>
-                      <option>Invisibility</option>
-                    </select>
-                  </div>
-                </label>
-              </div>
-              <div class="field">
-                <label class="checkbox" for="active">
-                  active
-                  <input
-                    type="checkbox"
-                    class="is-primary"
-                    id="active"
-                    v-model="hero.active"
-                  />
-                </label>
-              </div>
+              </label>
+            </div>
+            <div class="field" v-show="showMore">
+              <label class="label" for="lastName">last name</label>
+              <input
+                class="input"
+                id="lastName"
+                v-model="selectedHero.lastName"
+              />
+            </div>
+            <div class="field" v-show="showMore">
+              <label class="label" for="description">description</label>
+              <input
+                class="input"
+                id="description"
+                v-model="selectedHero.description"
+              />
             </div>
           </div>
-          <footer class="card-footer">
-            <button
-              class="link card-footer-item cancel-button"
-              @click="cancelHero"
-            >
-              <i class="fas fa-undo"></i>
-              <span>Cancel</span>
-            </button>
-            <button class="link card-footer-item save-button" @click="saveHero">
-              <i class="fas fa-save"></i>
-              <span>Save</span>
-            </button>
-          </footer>
         </div>
-        <div class="notification is-info">{{ message }}</div>
       </div>
     </div>
   </div>
 </template>
 
-<script lang="ts">
+<script>
 export default {
-  name: 'heros',
+  name: 'Heroes',
   data() {
     return {
-      hero: {
-        id: 20,
-        firstName: 'Madelyn',
-        lastName: 'Papa',
-        description: 'the cat whisperer',
-        capeColor: '',
-        power: '',
-        active: '',
-      },
-      message: 'sas',
+      selectedHero: null,
+      showMore: false,
+      heroes: [
+        {
+          id: 10,
+          firstName: 'Ella',
+          lastName: 'Papa',
+          description: 'fashionista',
+        },
+        {
+          id: 20,
+          firstName: 'Madelyn',
+          lastName: 'Papa',
+          description: 'the cat whisperer',
+        },
+        {
+          id: 30,
+          firstName: 'Haley',
+          lastName: 'Papa',
+          description: 'pen wielder',
+        },
+        {
+          id: 40,
+          firstName: 'Landon',
+          lastName: 'Papa',
+          description: 'arc trooper',
+        },
+      ],
     };
-  },
-  methods: {
-    cancelHero() {
-      this.message = '';
-    },
-    saveHero() {
-      this.message = JSON.stringify(this.hero, null, 2);
-    },
-    clearPower() {
-      this.hero.power = '';
-    },
   },
 };
 </script>
-
-<style scoped></style>
